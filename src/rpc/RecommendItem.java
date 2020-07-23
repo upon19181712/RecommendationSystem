23a59ad35a993bb6d8ebe9260e62c2a9c5408720
SuliLi <sulihhh@sulilis-mbp.myfiosgateway.com>
2020-07-23 19:07:52 -0400

    update add favotite movie

--- src/rpc/RecommendItem.java
+++ src/rpc/RecommendItem.java
@@ -1,16 +1,22 @@
 package rpc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Servlet implementation class RecommendItem
  */
-@WebServlet("/RecommendItem")
+@WebServlet("/recommendation")
 public class RecommendItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +32,26 @@ public class RecommendItem extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		 TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("application/json");
+		PrintWriter writer = response.getWriter();
+		
+		JSONArray array = new JSONArray();
+		
+		try {
+			array.put(new JSONObject().put("name","abcd").put("address","San").put("time","01/01/2017"));
+			array.put(new JSONObject().put("name","1234").put("address","San").put("time","01/01/2017"));
+			
+		}catch(JSONException e) {
+			e.printStackTrace();
+			
+		}
+		
+		RpcHelper.writeJsonArray(response,array);
+		
+//		writer.print(array);
+		
 	}
 
 	/**
--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -2,6 +2,7 @@ package rpc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,55 +14,64 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+
+import entity.Item;
+import external.TicketMasterClient;
+
 /**
  * Servlet implementation class SearchItem
  */
-@WebServlet("/Search")
+@WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("application/json");
-		PrintWriter writer = response.getWriter();
-		
-		JSONArray array = new JSONArray();
-		
-		
-			
-			
-			try {
-				
-				array.put(new JSONObject().put("username", "abcd"));
-				array.put(new JSONObject().put("username","1234"));
-				
-			}catch(JSONException e) {
-				e.printStackTrace();
-				
-			}	
-		
-		
-		writer.print(array);
-		
-		
+	public SearchItem() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		// response.setContentType("application/json");
+		// PrintWriter writer = response.getWriter();
+
+		// JSONArray array = new JSONArray();
+
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+
+		TicketMasterClient client = new TicketMasterClient();
+		
+		List<Item> items = client.search(lat, lon, null);
+		JSONArray array = new JSONArray();
+		
+		for(Item item : items) {
+			array.put(item.toJSONObject());
+		}
+		RpcHelper.writeJsonArray(response, array);
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+//		writer.print(array);
+
+	}
+	
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
